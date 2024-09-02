ff008cb8f81c4db024224db66b6cb95006d976e3
Carlos Eduardo Salazar Araujo <CarlosEduardo.Araujo@wexinc.com>
2024-09-02 14:39:49 -0300

    Create Seller Class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,9 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
@@ -9,6 +12,9 @@ public class Program {
         System.out.flush();
         
         Department obj = new Department(1, "Books");
+
+        Seller seller = new Seller(21, "Carlos", "csalazar@gmail.com", new Date(), 3000.00, obj);
         System.err.println(obj);
+        System.err.println(seller);
     }
 }
