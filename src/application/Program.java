a2378978d77e96c8a8569386b019f53341e6c23c
Carlos Eduardo Salazar Araujo <CarlosEduardo.Araujo@wexinc.com>
2024-09-04 17:57:29 -0300

    DepartmentDao implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -8,6 +8,7 @@ import java.util.List;
 import java.util.Scanner;
 
 import model.dao.DaoFactory;
+import model.dao.DepartmentDao;
 import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
@@ -20,53 +21,61 @@ public class Program {
         
         Scanner scan = new Scanner(System.in);
         DateFormat df = new SimpleDateFormat("MM-dd-yyyy");
-
-        // Department obj = new Department(1, "Books");
+        
         // Seller seller = new Seller(21, "Carlos", "csalazar@gmail.com", new Date(), 3000.00, obj);
 
-        SellerDao sellerDao = DaoFactory.createSellerDao();
-        System.out.println("<<< findByID() Validation >>>");
-        Seller seller = sellerDao.findById(1);
+        // ----------------------------
+        // Testing the SellerDao
+        // ----------------------------
+        // SellerDao sellerDao = DaoFactory.createSellerDao();
+        // System.out.println("<<< findByID() Validation >>>");
+        // Seller seller = sellerDao.findById(1);
 
-        System.out.println(seller);
+        // System.out.println(seller);
 
-        System.out.println("\n<<< findByDepartment() Validation >>>");
-        Department department = new Department(1, null);
-        List<Seller> list = sellerDao.findByDepartment(department);
+        // System.out.println("\n<<< findByDepartment() Validation >>>");
+        // Department department = new Department(1, null);
+        // List<Seller> list = sellerDao.findByDepartment(department);
         
-        for (Seller obj : list){
-            System.out.println(obj);
-        }
+        // for (Seller obj : list){
+        //     System.out.println(obj);
+        // }
 
-        System.out.println("\n<<< findAll() Validation >>>");
-        //List<Seller> listAll = sellerDao.findAll();
-        list = sellerDao.findAll();
+        // System.out.println("\n<<< findAll() Validation >>>");        
+        // list = sellerDao.findAll();
         
-        for (Seller obj : list){
-            System.out.println(obj);
-        }
+        // for (Seller obj : list){
+        //     System.out.println(obj);
+        // }
 
-        System.out.println("\n<<< Seller INSERT Validation >>>");
-        Date d1 = df.parse("09-19-1973");
-        //Seller newSeller = new Seller("Jean Hinton", "jhinton@wexinc.om", new Date(), 6500.00, department);
-        Seller newSeller = new Seller("Jean Hinton", "jhinton@wexinc.om", d1, 6500.00, department);
+        // System.out.println("\n<<< Seller INSERT Validation >>>");
+        // Date d1 = df.parse("09-19-1973");
+        // Seller newSeller = new Seller("Jean Hinton", "jhinton@wexinc.om", d1, 6500.00, department);
 
-        sellerDao.insert(newSeller);
-        System.out.println("Inserted New Id: " + newSeller.getId());
+        // sellerDao.insert(newSeller);
+        // System.out.println("Inserted New Id: " + newSeller.getId());
 
         
-        System.out.println("\n<<< Seller UPDATE Validation >>>");
-        seller = sellerDao.findById(5);
-        seller.setBase_salary(7300.00);
-        sellerDao.update(seller);
-        System.out.println("Update Completed!");
+        // System.out.println("\n<<< Seller UPDATE Validation >>>");
+        // seller = sellerDao.findById(5);
+        // seller.setBase_salary(7300.00);
+        // sellerDao.update(seller);
+        // System.out.println("Update Completed!");
 
-        System.out.println("\n<<< Seller DELETE Validation >>>");
-        System.out.print("Enter the ID to delete: ");
-        int idDel = scan.nextInt();
-        sellerDao.deleteById(idDel);
-        System.out.println("Delete completed.");
-        scan.close();
+        // System.out.println("\n<<< Seller DELETE Validation >>>");
+        // System.out.print("Enter the ID to delete: ");
+        // int idDel = scan.nextInt();
+        // sellerDao.deleteById(idDel);
+        // System.out.println("Delete completed.");
+        // scan.close();
+
+
+        // ----------------------------
+        // Testing the DepartmentDao
+        // ----------------------------
+        
+        Department obj = new Department(1, "Books");
+        DepartmentDao deptDao = DaoFactory.createDepartmentDao();
 
     }
 }
--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -1,6 +1,7 @@
 package model.dao;
 
 import db.DB;
+import model.dao.impl.DepartmentDaoJDBC;
 import model.dao.impl.SellerDaoJDBC;
 
 // Classe possui operações staticas para instaciar os Daos
@@ -9,5 +10,9 @@ public class DaoFactory {
     // abaixo nós temos a interface SellerDao que instancia uma implementação
     public static SellerDao createSellerDao(){
         return new SellerDaoJDBC(DB.getConnection());                     
+    }
+
+    public static DepartmentDao createDepartmentDao(){
+        return new DepartmentDaoJDBC();
     }
 }
